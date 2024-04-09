7ef6e8fb8d156f88dc7e8869f3cd498ce0066c12
Hicham Lahfairi <hicham.lahfairi@yahoo.de>
2024-04-09 18:53:44 +0200

    Add BoxValueCard

--- Assets/Scripts/Cards/BoxValueCard.cs
+++ Assets/Scripts/Cards/BoxValueCard.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 namespace com.deathbox.jam
 {
-    public class SeeThroughCard : ACard
+    public class BoxValueCard : ACard
     {
         public override void Execute(CardParam param)
         {
-            Debug.Log($"Is it a bad Box: {param.Box.IsBad}");
+            Debug.Log($"The value of the Box is : {param.Box.Amount}");
         }
     }
 }
